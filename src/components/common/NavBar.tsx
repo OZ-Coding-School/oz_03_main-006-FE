2424774b8bb7fef279a860bc86af53062117ae6b
h22jul22 <heeju9402@gmail.com>
2024-07-12 17:40:24 +0900

    feat: 로고 아이콘 반영

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,5 +1,10 @@
 const NavBar = () => {
-  return <div className='w-[392px] bg-[#28466A]'>NavBar</div>;
+  return (
+    <div className='w-[392px] bg-[#28466A]'>
+      NavBar
+      <img src='/logo.svg' />
+    </div>
+  );
 };
 
 export default NavBar;
