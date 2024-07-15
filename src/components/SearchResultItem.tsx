31fd7aa71309d9f1de502bdd1f9185503afbd701
h22jul22 <heeju9402@gmail.com>
2024-07-15 11:02:22 +0900

    feat: 사이드바 생성 및 UI 디자인(#4)

---
+++ src/components/SearchResultItem.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+interface SearchResultItemProps {
+  title: string;
+  body: string;
+  representative_image_id: string;
+  textColor: string;
+}
+
+const SearchResultItem: React.FC<SearchResultItemProps> = ({
+  title,
+  body,
+  representative_image_id,
+  textColor,
+}) => {
+  const [imgSrc, setImgSrc] = useState<string>(
+    representative_image_id ? representative_image_id : '/logo.svg'
+  );
+
+  const errorLogoWidth = imgSrc === '/logo.svg' ? 'w-12' : '';
+  const errorLogoHeight = imgSrc === '/logo.svg' ? 'h-12' : '';
+  const errorLogoMargin = imgSrc === '/logo.svg' ? 'm-4' : '';
+
+  const handleError = () => {
+    setImgSrc('/logo.svg');
+  };
+
+  const contentTruncate = (str: string, n: number) => {
+    return str?.length > n ? str.substring(0, n) + '...' : str;
+  };
+
+  return (
+    <div className='flex w-full gap-2'>
+      <div className='h-20 w-20 rounded-lg bg-[#F4F4F4]'>
+        <img
+          className={`rounded-lg ${errorLogoMargin} ${errorLogoWidth} ${errorLogoHeight}`}
+          src={imgSrc}
+          onError={handleError}
+          alt='게시글 썸네일'
+        />
+      </div>
+      <div className='flex w-[220px] flex-col gap-1'>
+        <h2 className={`mt-0.5 truncate text-lg font-semibold ${textColor}`}>
+          {title}
+        </h2>
+        <p className={`flex-grow text-justify text-sm ${textColor}`}>
+          {contentTruncate(body, 40)}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SearchResultItem;
