b0e27963cbc45a745e3ff6f292055217b0944753
KingBoRam <bory2321@naver.com>
2024-07-15 11:19:26 +0900

    feat: main page background에 naver map 적용

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from 'react';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  useEffect(() => {
+    const mapOptions = {
+      center: new naver.maps.LatLng(36.3595704, 127.105399),
+      zoom: 7,
+      disableDoubleClickZoom: true,
+      disableDoubleTapZoom: true,
+      maxZoom: 13,
+      minZoom: 7,
+    };
+    const map = new naver.maps.Map('map', mapOptions);
+  }, []);
+
+  return <div id='map' className='h-full w-full'></div>;
 };
 
 export default HomePage;
