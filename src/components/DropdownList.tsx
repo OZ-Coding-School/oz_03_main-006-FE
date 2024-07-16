2afb683aaa6baf9ae8cca90e243dc11ff6848bbf
KingBoRam <bory2321@naver.com>
2024-07-16 18:29:32 +0900

    feat: marker animation

--- src/components/DropdownList.tsx
+++ src/components/DropdownList.tsx
@@ -4,18 +4,21 @@ import { locationList } from '../data/locationList';
 
 interface DropdownListProps {
   map: naver.maps.Map | null;
+  marker: naver.maps.Marker[] | null;
 }
 
-const DropdownList: React.FC<DropdownListProps> = ({ map }) => {
+const DropdownList: React.FC<DropdownListProps> = ({ map, marker }) => {
+  console.log(marker);
   const timeoutRef = useRef<number | null>(null);
 
+  // 해당 하는 지역으로 zoomin + move
   const moveCenter = (location: string) => {
     const selectedLocation = locationList.find(
       (item) => item.name === location
     );
-
     if (selectedLocation && map) {
       if (timeoutRef.current) {
+        // 다른 지역으로 이동했을 때 기존의 이벤트 삭제
         clearTimeout(timeoutRef.current);
       }
       timeoutRef.current = window.setTimeout(() => {
@@ -23,11 +26,12 @@ const DropdownList: React.FC<DropdownListProps> = ({ map }) => {
           selectedLocation.latitude,
           selectedLocation.longitude
         );
-        map.morph(boundsLocation, 9);
-      }, 500);
+        map.morph(boundsLocation, 10);
+      }, 300);
     }
   };
 
+  // 지도 이동 하기 전에, 또 다른 지역이 아니라 아예 리스트 밖으로 나갔을 때 이벤트 삭제
   const clearMoveCenterTimeout = () => {
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
@@ -39,14 +43,14 @@ const DropdownList: React.FC<DropdownListProps> = ({ map }) => {
     <div className='absolute right-[3%] top-[3%]'>
       <ul className='flex w-[201px] flex-col overflow-hidden rounded-b-lg rounded-t-lg bg-[#F5F5F5] bg-opacity-60'>
         <li className='mb-1 flex items-center justify-between p-4 pb-0'>
-          접기<img src=''></img>
+          접기
           <IoMdArrowDropup />
         </li>
         <div className='my-2 h-[1px] w-full bg-[#575757] bg-opacity-10'></div>
         {locationList.map((item, index) => (
           <li
             key={index}
-            className='mb-2 flex items-center pl-4 font-chosun hover:bg-[#575757] hover:bg-opacity-10'
+            className='mb-2 flex items-center pl-4 hover:bg-[#575757] hover:bg-opacity-10'
             onMouseEnter={() => moveCenter(item.name)}
             onMouseLeave={clearMoveCenterTimeout}
           >
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,28 +1,46 @@
 import { useState, useEffect } from 'react';
 import DropdownList from '../components/DropdownList';
+import { locationList } from '../data/locationList';
 
 const HomePage = () => {
   const [map, setMap] = useState<naver.maps.Map | null>(null);
   const centerX = 36.3595704;
   const centerY = 127.105399;
+  const [marker, setMarker] = useState<naver.maps.Marker[] | null>(null);
 
+  // 지도 생성
   useEffect(() => {
     const mapOptions = {
       center: new naver.maps.LatLng(centerX, centerY),
       zoom: 7,
       disableDoubleClickZoom: true,
       disableDoubleTapZoom: true,
-      maxZoom: 10,
+      maxZoom: 12,
       minZoom: 7,
     };
     const mapInstance = new naver.maps.Map('map', mapOptions);
     setMap(mapInstance);
   }, []);
 
+  // 마커생성하고 마커를 담은 배열을 state로 관리 -> dropdownlist로 넘겨서 list 특정지역 호버시 해당 지역의 마커 bound animation 줄 예정
+  useEffect(() => {
+    if (map) {
+      const MarkerArr = locationList.map((item) => {
+        return new naver.maps.Marker({
+          position: new naver.maps.LatLng(item.latitude, item.longitude),
+          map: map,
+          animation: naver.maps.Animation.DROP,
+          title: item.name,
+        });
+      });
+      setMarker(MarkerArr);
+    }
+  }, [map]);
+
   return (
     <>
       <div id='map' className='relative h-full w-full'></div>
-      <DropdownList map={map}></DropdownList>
+      <DropdownList map={map} marker={marker}></DropdownList>
     </>
   );
 };
