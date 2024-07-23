60c55ab4a2ed032e2db608ff53b8e47008a27043
h22jul22 <heeju9402@gmail.com>
2024-07-23 16:40:11 +0900

    fix: root 스크롤바 없애기

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -26,28 +26,24 @@ const LoginPage = () => {
     activeTab === 'signUp' ? 'signUp-button-shadow' : '';
 
   return (
-    <div className='h-screen w-screen'>
-      <div className='fixed flex h-full w-full items-center justify-center'>
-        <div className='h-[640px] w-[460px] bg-transparent'>
-          <div className='flex h-full w-full flex-col px-8 py-4'>
-            <div className='grid h-[50px] w-full grid-cols-2 items-center gap-0.5 text-center'>
-              <div
-                className={`login-button-radius ${handleLoginShadow} ${handleAnimation} flex h-full cursor-pointer items-center justify-center bg-[#f9f9f9]`}
-                onClick={() => handleTabClick('login')}
-              >
-                <p className='font-chosun text-lg font-semibold'>로그인</p>
-              </div>
-              <div
-                className={`signUp-button-radius ${handleSignUpShadow} ${handleAnimation} light-white flex h-full cursor-pointer items-center justify-center bg-[#28466A]`}
-                onClick={() => handleTabClick('signUp')}
-              >
-                <p className='font-chosun text-lg font-semibold'>회원가입</p>
-              </div>
-            </div>
-            <div className={`grow ${handleAnimation}`}>
-              {activeTab === 'login' ? <Login /> : <SignUp />}
-            </div>
+    <div className='fixed flex h-screen w-screen items-center justify-center'>
+      <div className='flex h-[640px] w-[460px] flex-col px-8 py-4'>
+        <div className='grid h-[50px] w-full grid-cols-2 items-center gap-0.5 text-center'>
+          <div
+            className={`login-button-radius ${handleLoginShadow} ${handleAnimation} flex h-full cursor-pointer items-center justify-center bg-[#f9f9f9]`}
+            onClick={() => handleTabClick('login')}
+          >
+            <p className='font-chosun text-lg font-semibold'>로그인</p>
           </div>
+          <div
+            className={`signUp-button-radius ${handleSignUpShadow} ${handleAnimation} light-white flex h-full cursor-pointer items-center justify-center bg-[#28466A]`}
+            onClick={() => handleTabClick('signUp')}
+          >
+            <p className='font-chosun text-lg font-semibold'>회원가입</p>
+          </div>
+        </div>
+        <div className={`grow ${handleAnimation}`}>
+          {activeTab === 'login' ? <Login /> : <SignUp />}
         </div>
       </div>
     </div>
